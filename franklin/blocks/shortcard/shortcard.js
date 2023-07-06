d2a70ffc5823b187f90e4b5f6a0b8259591b06d1
redvelvet511 <113856982+redvelvet511@users.noreply.github.com>
2023-07-06 16:22:24 -0400

    Changed libs

--- franklin/blocks/shortcard/shortcard.js
+++
@@ -1,3 +0,0 @@
-export default function init(block) {
-  console.log(block);
-}
--- franklin/scripts/scripts/scripts.js
+++ franklin/scripts/scripts/scripts.js
@@ -16,7 +16,7 @@ import { setLibs, createTag } from './utils.js';
 const STYLES = '/franklin/styles/styles.css';
 
 // Use '/libs' if your live site maps '/libs' to milo's origin.
-const LIBS = 'https://milo.adobe.com/libs';
+const LIBS = 'https://shortcard--milo--wbstry.hlx.live/libs';
 
 // Add any config options.
 const CONFIG = {
